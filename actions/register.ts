d285898deae30bb2b50da5db90b19f95dfdb26ea
Timo Heman <timoheman16@gmail.com>
2024-07-09 15:26:45 +0100

    added register form

---
+++ actions/register.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import * as z from "zod";
+
+import { RegisterSchema } from "@/schemas";
+
+export async function register(values: z.infer<typeof RegisterSchema>) {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid Credentials" };
+  }
+
+  return { success: "Email Sent!" };
+}
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -4,8 +4,8 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import { LoginSchema } from "@/schemas";
-import { login } from "@/actions/login";
+import { RegisterSchema } from "@/schemas";
+import { register } from "@/actions/register";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import {
   Form,
@@ -21,24 +21,25 @@ import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 import { useState, useTransition } from "react";
 
-export function LoginForm() {
+export function RegisterForm() {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof LoginSchema>) {
+  function onSubmit(values: z.infer<typeof RegisterSchema>) {
     setError("");
     setSuccess("");
 
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setError(data.error);
         setSuccess(data.success);
       });
@@ -48,14 +49,32 @@ export function LoginForm() {
   return (
     <div>
       <CardWrapper
-        headerLabel="Welcome back"
-        backButtonHref="/auth/register"
-        backButtonLabel="Don't have an account?"
+        headerLabel="Create an account"
+        backButtonHref="/auth/login"
+        backButtonLabel="Already have an account?"
         showSocial
       >
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
             <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="John Doe"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
               <FormField
                 control={form.control}
                 name="email"
@@ -97,7 +116,7 @@ export function LoginForm() {
             <FormError message={error} />
             <FormSuccess message={success} />
             <Button disabled={isPending} type="submit" className="w-full">
-              Login
+              Sign Up
             </Button>
           </form>
         </Form>
